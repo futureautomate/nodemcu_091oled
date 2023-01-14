e40eababd8f95358fc76a0fcbe58087f3ca8e911
Tejas <futureautomate@gmail.com>
2023-01-14 13:55:13 +0530

    Added COde files

---
+++ Hello World.ino
@@ -0,0 +1,38 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSansBold9pt7b.h >
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000);
+
+  display.clearDisplay();
+
+  display.setTextColor(WHITE);
+  display.setCursor(10, 20);
+  display.setTextSize(1);
+  display.setFont(&FreeSansBold9pt7b);
+  display.println("Subscribe!!");
+  display.display();
+  delay(2000);
+}
+
+void loop() {
+  // Nothing to do here
+}
